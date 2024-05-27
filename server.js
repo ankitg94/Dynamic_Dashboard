d7668e026cd9d2814b4896aa1c4beb32922641f6
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2024-05-27 20:58:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv"
 import { dbConnect } from "./config/db.js"
 import dataRoute from "./routes/dataRoute.js"
 
+//step-1(host)
+import path from 'path'
+import { fileURLToPath } from "url";
 dbConnect()
 //dotenv config
 dotenv.config()
@@ -11,6 +14,10 @@ dotenv.config()
 //mongodb connection 
 
 dbConnect()
+//02-hosting k liye(start)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+// hositng (end)
 //rest object 
 const app =express()
 
@@ -19,9 +26,18 @@ app.use(cors())
 app.use(express.json())
 
 
+//03-hoisting(start)
+app.use(express.static(path.join(__dirname, './client/build')))
+// hositng (end)
 //routes
 //app.get('/',(req,res)=>{})
 app.use("/api/v1/data",dataRoute)
+
+//04-hoisting(start)
+app.use("*",function(req,res){
+       res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
+// hositng (end)
 
 //port
 const port =process.env.PORT || 8080
